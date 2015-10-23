27d892729be6928bacdc3c1b73d1c7a36e665324
Jack Invictus <jackinvictus@outlook.com>
2015-10-22 23:01:42 -0700

    GUI optimize

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Remote Control' });
+  res.render('index', { title: 'Cisco Light Conservation' });
 });
 
 /* GET View page */
 router.get('/view', function(req, res, next) {
-  res.render('view', { title: 'Remote Control' });
+  res.render('view', { title: 'Cisco Light Conservation' });
 });
 
 /* GET View page */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Remote Control' });
+  res.render('about', { title: 'Cisco Light Conservation' });
 });
 
 module.exports = router;
